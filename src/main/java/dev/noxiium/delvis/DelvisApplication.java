fa02537645210587bb5aefe7a5d7eb7ec64909d9
Noxiium <johansf98@hotmail.com>
2024-07-07 15:40:41 +0200

    initial commit, spring-boot setup

---
+++ src/main/java/dev/noxiium/delvis/DelvisApplication.java
@@ -0,0 +1,13 @@
+package dev.noxiium.delvis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelvisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelvisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/noxiium/delvis/DelvisApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.noxiium.delvis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelvisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
